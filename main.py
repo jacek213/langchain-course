0a670e53ffcd98845e3851519e7a08a5b46be2cc
Jacek Grzybowski <jacek.grzybowski@example.com>
2025-09-04 23:36:12 +0200

    search agent

--- main.py
+++ main.py
@@ -26,8 +26,8 @@ def main():
         input_variables=["information"], template=summary_template
     )
 
-    # llm = ChatOpenAI(temperature=0, model="gpt-5")
-    llm = ChatOllama(temperature=0, model="gemma3:270m")
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    # llm = ChatOllama(temperature=0, model="gemma3:270m")
     chain = summary_prompt_template | llm
     response = chain.invoke(input={"information": information})
 
